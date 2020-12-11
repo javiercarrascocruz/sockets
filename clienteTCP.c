2a1bba77a9374c17b5a19906ae11c9cc1cfa35d5
javier.carrasco.cruz@gmail.com <javier.carrasco.cruz@gmail.com>
2020-12-11 14:30:58 +0100

    cliente y servidor TCP con comunicacion entre ellos

--- clienteTCP.c
+++ clienteTCP.c
@@ -5,35 +5,37 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+#define SIZE 255
 
 int main(int argc, char *argv[])
 {
-	int mi_socket, nuevo, tam;
+	int mi_socket, tam, numbytes;
+	char buffer[SIZE];
 	struct sockaddr_in mi_estructura;
 	
+	if(argc != 3)
+	{
+		printf("error: modo de empleo: clienttcp ip puerto\n");
+		exit (-1);
+	}
+	
 	mi_estructura.sin_family = AF_INET;
-	mi_estructura.sin_port = 0;
-	mi_estructura.sin_addr.s_addr = INADDR_ANY;
+	mi_estructura.sin_port = htons(atoi(argv[2]));
+	inet_aton(argv[1], &(mi_estructura.sin_addr));
 	memset(&(mi_estructura.sin_zero), '\0', 8);
+	
 	mi_socket = socket(AF_INET, SOCK_STREAM, 0);
 	tam = sizeof(struct sockaddr);
-	bind(mi_socket, (struct sockaddr *)&mi_estructura, tam);
-	listen(mi_socket, 5);
-	while(1)
-	{
-		nuevo = accept(mi_socket, (struct sockaddr *)&mi_estructura, &tam);
-		if(fork() == 0)
-		{	//hijo
-			close(mi_socket);	//proceso hijo no lo necesita
-			send(nuevo, "200 Bienvenido\n", 15, 0);
-			close(nuevo);
-			exit(0);
-		} 
-		else	//padre
-		{
-			close(nuevo);	//proceso padre no lo necesita
-		}
-	}
+	connect(mi_socket, (struct sockaddr *)&mi_estructura, tam);
+	
+	numbytes = recv(mi_socket, buffer, SIZE-1, 0);
+	buffer[numbytes] = '\0';
+	printf("%d bytes recibidos\n", numbytes);
+	printf("recibido: %s\n", buffer);
+	close(mi_socket);
+	
 	return 0;
 }
 
