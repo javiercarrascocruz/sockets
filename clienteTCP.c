91837fb03daff4115ac87edbe86a2607db9f24c8
javier.carrasco.cruz@gmail.com <javier.carrasco.cruz@gmail.com>
2020-12-11 14:54:18 +0100

    Mayúsculas en los textos

--- clienteTCP.c
+++ clienteTCP.c
@@ -14,28 +14,27 @@ int main(int argc, char *argv[])
 	int mi_socket, tam, numbytes;
 	char buffer[SIZE];
 	struct sockaddr_in mi_estructura;
-	
+
 	if(argc != 3)
 	{
-		printf("error: modo de empleo: clienttcp ip puerto\n");
+		printf("Error: modo de empleo: clienttcp ip puerto\n");
 		exit (-1);
 	}
-	
+
 	mi_estructura.sin_family = AF_INET;
 	mi_estructura.sin_port = htons(atoi(argv[2]));
 	inet_aton(argv[1], &(mi_estructura.sin_addr));
 	memset(&(mi_estructura.sin_zero), '\0', 8);
-	
+
 	mi_socket = socket(AF_INET, SOCK_STREAM, 0);
 	tam = sizeof(struct sockaddr);
 	connect(mi_socket, (struct sockaddr *)&mi_estructura, tam);
-	
+
 	numbytes = recv(mi_socket, buffer, SIZE-1, 0);
 	buffer[numbytes] = '\0';
 	printf("%d bytes recibidos\n", numbytes);
-	printf("recibido: %s\n", buffer);
+	printf("Recibido: %s\n", buffer);
 	close(mi_socket);
-	
+
 	return 0;
 }
-
